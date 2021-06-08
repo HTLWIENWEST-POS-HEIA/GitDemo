b29a9440bc83c040e6bf514b79effe50612a325e
listenprojekt <andreas.heinbach@htl-ottakring.ac.at>
2021-06-08 18:19:13 +0200

    Programm im Anfangszustand

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public double plus(double a, double b){
+        return 0;
+    }
+    public double minus(double a, double b){
+        return 0;
+    }
+    public double mult(double a, double b){
+        return 0;
+    }
+    public double divid(double a, double b){
+        return 0;
+    }
+}
