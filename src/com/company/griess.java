2665cf859b2bc72ab73de4fa3c2853cc8ae7309a
griess.p19 <griess.p19@htlwienwest.at>
2021-06-21 18:09:04 +0200

    neues File Philipp

---
+++ src/com/company/griess.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+// Griess kann addieren....
+    public static void main(String[] args) {
+	// write your code here
+    }
+    public double plus(double a, double b){
+        return a+b;
+    }
+}
